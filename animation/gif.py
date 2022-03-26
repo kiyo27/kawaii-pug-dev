8af8d0d7f80b91204c4a6ef285d6b59034c14f73
kiyo27 <19531773+kiyo27@users.noreply.github.com>
2022-03-27 04:19:48 +0900

    arrangement directory structure

--- animation/gif.py
+++ animation/gif.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 
 images = []
-images.append(Image.open('ball1.png'))
-images.append(Image.open('ball2.png'))
+images.append(Image.open('ball1.png').resize((256, 256), Image.BOX))
+images.append(Image.open('ball2.png').resize((256, 256), Image.BOX))
 images[0].save('anime.gif', save_all=True, append_images=images[1:2], duration=100, loop=0)
 
