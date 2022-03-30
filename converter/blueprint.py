0e201e4c9d545b6eb24e63090853b5cdfdb770c8
kiyo27 <19531773+kiyo27@users.noreply.github.com>
2022-03-31 00:49:56 +0900

    refactoring

--- converter/blueprint.py
+++ converter/blueprint.py
@@ -5,9 +5,65 @@ from abc import ABCMeta, abstractmethod
 
 
 class Attributes:
-    
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self, obj, **kwargs):
+        self.base_dir = 'blueprints/attributes/'
+        self._obj = obj
+        self.make(**kwargs)
+
+    @property
+    def face(self):
+        return self._face
+
+    @property
+    def head(self):
+        return self._head
+
+    @property
+    def mouth(self):
+        return self._mouth
+
+    @property
+    def eyes(self):
+        return self._eyes
+
+    @property
+    def ears(self):
+        return self._ears
+
+    def make(self, **kwargs):
+        self.makeFace(**kwargs)
+        self.makeHead(**kwargs)
+        self.makeNeck(**kwargs)
+        self.makeMouth(**kwargs)
+        self.makeEyes(**kwargs)
+        self.makeEars(**kwargs)
+
+    def makeFace(self, **kwargs):
+        if 'face' in kwargs:
+            self._face = kwargs['face']
+        else:
+            self._face = None
+
+    def makeHead(self, **kwargs):
+        self._head = None
+
+    def makeNeck(self, **kwargs):
+        self._neck = None
+
+    def makeMouth(self, **kwargs):
+        if 'mouth' in kwargs:
+            self._mouth = kwargs['mouth']
+        else:
+            self._mouth = None
+
+    def makeEyes(self, **kwargs):
+        if self.face is not None:
+            self._eyes = None
+
+        self._eyes = None
+
+    def makeEars(self, **kwargs):
+        self._ears = None
 
 
 class Shape(metaclass=ABCMeta):
@@ -26,13 +82,15 @@ class Shape(metaclass=ABCMeta):
     def mouth(self):
         pass
 
+    @abstractmethod
+    def make(self, **kwargs):
+        pass
+
 
 class PugShape(Shape):
     def __init__(self, **kwargs):
-        bp_base_dir = 'blueprints/base/shape/'
-        self._edge = bp_base_dir + 'edge/basic.csv'
-        self._eyes = bp_base_dir + 'eyes/basic.csv'
-        self._mouth = bp_base_dir + 'mouth/basic.csv'
+        self.base_dir = 'blueprints/base/shape/'
+        self.make(**kwargs)
 
     @property
     def edge(self):
@@ -46,7 +104,23 @@ class PugShape(Shape):
     def mouth(self):
         return self._mouth
 
+    def make(self, **kwargs):
+        self.makeEdge(**kwargs)
+        self.makeEyes(**kwargs)
+        self.makeMouth(**kwargs)
+
+    def makeEdge(self, **kwargs):
+        self._edge = self.base_dir + 'edge/basic.csv'
+        
+    def makeEyes(self, **kwargs):
+        self._eyes = self.base_dir + 'eyes/basic.csv'
+        
+    def makeMouth(self, **kwargs):
+        self._mouth = self.base_dir + 'mouth/basic.csv'
+
+
 class Color(metaclass=ABCMeta):
+
     @property
     @abstractmethod
     def base(self):
@@ -62,13 +136,15 @@ class Color(metaclass=ABCMeta):
     def ears(self):
         pass
 
+    @abstractmethod
+    def make(self, **kwargs):
+        pass
+
 
 class PugColor(Color):
     def __init__(self, **kwargs):
-        bp_base_dir = 'blueprints/base/color/'
-        self._base = bp_base_dir + 'base/basic.csv'
-        self._eyes = None
-        self._ears = None
+        self.base_dir = 'blueprints/base/color/'
+        self.make(**kwargs)
 
     @property
     def base(self):
@@ -82,34 +158,19 @@ class PugColor(Color):
     def ears(self):
         return self._ears
 
-
-class Factory(metaclass=ABCMeta):
-    @abstractmethod
-    def createShape(self, **kwargs):
-        pass
-
-    @abstractmethod
-    def createColor(self, **kwargs):
-        pass
-
-    @abstractmethod
-    def createAttributes(self, **kwargs):
-        pass
-
-
-class PugFactory(Factory):
-    
-    def createShape(self, **kwargs):
-        return PugShape(**kwargs)
-
-    def createColor(self, **kwargs):
-        return PugColor(**kwargs)
-
-    def createAttributes(self, **kwargs):
-        return Attributes(**kwargs)
+    def make(self, **kwargs):
+        self._base = self.base_dir + 'base/basic.csv' 
+        self._eyes = None
+        self._ears = None
 
 
 class Character(metaclass=ABCMeta):
+    @classmethod
+    def get_factory(cls, classname):
+        mod = __import__(__name__)
+        factory = getattr(mod, classname)
+        return factory()
+
     @property
     @abstractmethod
     def shape(self):
@@ -125,16 +186,13 @@ class Character(metaclass=ABCMeta):
     def attributes(self):
         pass
 
+    @abstractmethod
+    def create(self, **kwargs):
+        pass
+
 
 class Pug(Character):
     """Register pug's blueprints."""
-
-    def __init__(self, **kwargs):
-        factory = PugFactory()
-        self._shape = factory.createShape(**kwargs)
-        self._color = factory.createColor(**kwargs)
-        self._attr = factory.createAttributes(**kwargs)
-
     @property
     def shape(self):
         return self._shape
@@ -147,14 +205,28 @@ class Pug(Character):
     def attributes(self):
         return self._attr
 
+    def create(self, **kwargs):        
+        kwargs.setdefault('shape', {})
+        kwargs.setdefault('color', {})
+        kwargs.setdefault('attributes', {})
+
+        self.makeShape(**kwargs['shape'])
+        self.makeColor(**kwargs['color'])
+        self.makeAttributes(**kwargs['attributes'])
+
+    def makeShape(self,**kwargs):
+        self._shape = PugShape(**kwargs)
+        
+    def makeColor(self,**kwargs):
+        self._color = PugColor(**kwargs)
+
+    def makeAttributes(self,**kwargs):
+        self._attr = Attributes(self, **kwargs)
+
 
 class AnonyPug(Pug):
-    """
-    Register AnonyPug's blueprints.
-    """
-    def __init__(self, **kwargs):
-        self._shape = PugShape()
-        self._color = PugColor()
-        self._attr = Attributes(face='anonymous')
-
+    """Register pug's blueprints."""
+    def makeAttributes(self, **kwargs):        
+        kwargs['face'] = 'anonymous'
+        self._attr = Attributes(self, **kwargs)
 
--- converter/tests/test_blueprint.py
+++ converter/tests/test_blueprint.py
@@ -3,7 +3,13 @@ import blueprint as bp
 
 class TestPugMethods(unittest.TestCase):
     def test_pug(self):
-        p = bp.Pug()
-        print(p.shape.edge)
+        p = bp.Character.get_factory('Pug')
+        d = {}
+        p.create(**d)
+        self.assertEqual(p.attributes.mouth, None)
 
-
+    def test_anonypug(self):
+        p = bp.Character.get_factory('AnonyPug')
+        d = {'attributes':{'mouth':'cigarette'}}
+        p.create(**d)
+        self.assertEqual(p.attributes.mouth, 'cigarette')
