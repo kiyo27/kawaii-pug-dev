13308de04216beb21a77d23c6b13c709aadd19d6
kiyo27 <19531773+kiyo27@users.noreply.github.com>
2022-04-16 17:52:14 +0900

    add command line parser

--- gen.py
+++ gen.py
@@ -1,41 +1,52 @@
 import cv2 as cv
 from PIL import Image
+import argparse
 from pixart import canvas, parser
 from pixart.helper import factory
 from pixart.io import sqlite, opencv
 
-#f = factory.Multiple()
-#face_types = ['Pug', 'SleepingPug']
-#f.create(face_types, 'character', 1, total=1000)
-#c = f.created[1]
 
-from character import types
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-m", action="store_true", help="multiple create")
+group.add_argument("-c", action="store_true", help="create at once")
+group.add_argument("--from-db", dest="db", action="store_true", help="create from db")
 
-#p = types.KabukiPug()
-#attr = {}
-#p.create(1, **attr)
-#
-#height = 24
-#width = 24
-#
-#painter = canvas.Painter(width, height)
-#
-#painter.draw(p)
-#out_file = 'img/output.png'
-#opencv.generate(painter.canvas, out_file)
-#img = Image.open(out_file)
-#img.show()
+
+def multiple():
+    f = factory.Multiple()
+    face_types = ['Pug', 'SleepingPug', 'KabukiPug']
+    f.create(face_types, 'character', 1, total=1000)
+    #c = f.created[1]
+    height = 24
+    width = 24
+    painter = canvas.Painter(width, height)
+    for p in f.created:
+        painter.drawColor(p)
+        painter.drawShape(p)
+        painter.drawAttributes(p)
+        opencv.generate(painter.canvas, out_file)
+
+
+def create():
+    from character import types
+    p = types.KabukiPug()
+    attr = {}
+    p.create(1, **attr)
+
+    height = 24
+    width = 24
+
+    painter = canvas.Painter(width, height)
+
+    painter.draw(p)
+    out_file = 'img/output.png'
+    opencv.generate(painter.canvas, out_file)
+    img = Image.open(out_file)
+    img.show()
 
 #sqlite.export_csv()
 
-#for p in f.created:
-#    painter.drawColor(p)
-#    painter.drawShape(p)
-#    painter.drawAttributes(p)
-#
-#    interpolation = cv.INTER_AREA
-#    canvas = cv.resize(painter.canvas, None, 0, 10,10,interpolation)
-#    cv.imwrite('output.png', canvas)
 
 #img = Image.open('img/output.png')
 #img.show()
@@ -56,4 +67,10 @@ def create_from_db():
 
 
 if __name__ == '__main__':
-    create_from_db()
+    args = parser.parse_args()
+    if args.m:
+        multiple()
+    elif args.c:
+        create()
+    elif args.db:
+        create_from_db()
